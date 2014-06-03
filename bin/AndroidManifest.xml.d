1e5edcb17bb1ef474dbf0aaaa0642090eab07ec0
Raja Rao DV <rajaraodv100@yahoo.com>
2014-06-02 17:33:30 -0700

    first commit

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,3 @@
+/Users/rraodv/apps/HelloAndroid/bin/AndroidManifest.xml : \
+/Users/rraodv/apps/HelloAndroid/AndroidManifest.xml \
+/Users/rraodv/apps/forcedroid/native/SalesforceSDK/AndroidManifest.xml \
---
+++ bin/HelloAndroidApp-debug-unaligned.apk.d
@@ -0,0 +1,10 @@
+/Users/rraodv/apps/HelloAndroid/bin/HelloAndroidApp-debug-unaligned.apk : \
+/Users/rraodv/apps/HelloAndroid/bin/HelloAndroidApp.ap_ \
+/Users/rraodv/apps/HelloAndroid/bin/classes.dex \
+/Users/rraodv/apps/forcedroid/native/SalesforceSDK/bin/classes.jar \
+/Users/rraodv/apps/forcedroid/native/SalesforceSDK/libs/httpmime-4.0.3.jar \
+/Users/rraodv/apps/forcedroid/native/SalesforceSDK/libs/volley_android-4.3_r2.1.jar \
+/Users/rraodv/apps/forcedroid/native/SalesforceSDK/libs/cordova-2.3.0.jar \
+/Users/rraodv/apps/forcedroid/native/SalesforceSDK/libs/apache-mime4j-0.6.jar \
+/Users/rraodv/apps/forcedroid/native/SalesforceSDK/libs/android-junit-report-1.2.6.jar \
+/Users/rraodv/apps/forcedroid/native/SalesforceSDK/libs/guava-r09.jar \
